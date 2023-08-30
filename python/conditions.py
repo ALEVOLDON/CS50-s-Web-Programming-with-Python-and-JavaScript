5437cad681ce7ebdbc712a42d602ba71443bf9be
Vladimir Rybalsky <34063175+ALEVOLDON@users.noreply.github.com>
2023-08-30 21:25:15 +0300

    fix error

--- python/conditions.py
+++ python/conditions.py
@@ -1,7 +1,8 @@
-num = input("Number: ")
+num = int(input("Number: "))
+
 if num > 0:
     print("Number is positive")
 elif num < 0:
     print("Number is negative")
 else:
-    print("Number is 0")
+    print("Number is zero")
