c7ca81ba8c6de0752614e86428df228ef75ab755
Vladimir Rybalsky <34063175+ALEVOLDON@users.noreply.github.com>
2023-09-04 20:34:28 +0300

    add sequences

---
+++ python/sequences.py
@@ -0,0 +1,47 @@
+# name = "Harry"
+# print(name[0])
+# print(name[1])
+
+# This is a Python comment
+# names = ["Harry", "Ron", "Hermione"]
+# Print the entire list:
+# print(names)
+# Print the second element of the list:
+# print(names[0])
+# Add a new name to the list:
+# names.append("Draco")
+# Sort the list:
+# names.sort()
+# Print the new list:
+# print(names)
+
+# Tuples
+# pointX = 12.5
+# pointY = 10.6
+# print(pointX, pointY)
+
+# point = (12.5,10.6)
+# print(point)
+
+# Sets
+
+# Create an empty set:
+s = set()
+
+# Add some elements:
+s.add(1)
+s.add(2)
+s.add(3)
+s.add(4)
+s.add(3)
+s.add(1)
+
+# Remove 2 from the set
+s.remove(2)
+
+# Print the set:
+print(s)
+
+# Find the size of the set:
+print(f"The set has {len(s)} elements.")
+
